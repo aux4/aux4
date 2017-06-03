d5129af795cc7a7a926666c6a6756a3c18bb02ac
David Sobreira Gouvea <davidsgbr@gmail.com>
2017-06-03 04:45:01 +0200

    Add profile

---
+++ test/profile.test.js
@@ -0,0 +1,95 @@
+const Profile = require('../lib/profile');
+
+const config = require('../lib/config');
+
+describe('profile', () => {
+  let configProfiles;
+  beforeEach(() => {
+    configProfiles = {
+      profiles: [
+        {
+          name: 'firstProfile',
+          commands: [
+            {
+              value: 'cmd',
+              execute: ['mkdir first', 'cd first']
+            }
+          ]
+        },
+        {
+          name: 'secondProfile',
+          commands: [
+            {
+              value: 'cmd',
+              execute: ['mkdir first', 'cd first']
+            }
+          ]
+        }
+      ]
+    };
+
+    config.file = jest.fn().mockReturnValue(configProfiles);
+  });
+
+  describe('create firstProfile', () => {
+    let firstProfile, firstProfileName;
+    beforeEach(() => {
+      firstProfileName = 'firstProfile';
+      firstProfile = new Profile(firstProfileName);
+    });
+
+    describe('firstProfile name', () => {
+      it('returns "firstProfile"', () => {
+        expect(firstProfile.name()).toEqual(firstProfileName);
+      });
+    });
+
+    describe('firstProfile commands', () => {
+      it('returns firstProfile commands', () => {
+        expect(firstProfile.commands()).toBe(configProfiles.profiles[0].commands);
+      });
+    });
+
+    describe('get "cmd" command', () => {
+      it('returns cmd command', () => {
+        expect(firstProfile.command('cmd')).toBe(firstProfile.commands[0]);
+      });
+    });
+
+    describe('get non-existent command', () => {
+      it('returns undefined', () => {
+        expect(firstProfile.command('x')).toBeUndefined();
+      });
+    });
+  });
+
+  describe('create secondProfile', () => {
+    let secondProfile, secondProfileName;
+    beforeEach(() => {
+      secondProfileName = 'secondProfile';
+    	secondProfile = new Profile(secondProfileName);
+    });
+
+    describe('secondProfile name', () => {
+      it('returns "secondProfile"', () => {
+        expect(secondProfile.name()).toEqual(secondProfileName);
+      });
+    });
+
+    describe('secondProfile commands', () => {
+      it('returns secondProfile commands', () => {
+        expect(secondProfile.commands()).toBe(configProfiles.profiles[1].commands);
+      });
+    });
+  });
+
+  describe('create profile which is not on the config properties', () => {
+    describe('thirdProfile', () => {
+      it('returns undefined', () => {
+        expect(() => {
+          new Profile('thirdProfile');
+        }).toThrow('profile thirdProfile not found in the configuration file');
+      });
+    });
+  });
+});
