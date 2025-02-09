efcf7f8abd07ef5d2ebc3ab6728de780995de558
dgouvea <david@pairingwith.me>
2025-02-09 07:44:53 -0800

    add flags to version

--- engine/executor/aux4.go
+++ engine/executor/aux4.go
@@ -1,18 +1,35 @@
 package executor
 
 import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
 	"aux4.dev/aux4/aux4"
 	"aux4.dev/aux4/core"
 	"aux4.dev/aux4/engine"
 	"aux4.dev/aux4/engine/param"
 	"aux4.dev/aux4/output"
-	"time"
 )
 
 type Aux4VersionExecutor struct {
 }
 
 func (executor *Aux4VersionExecutor) Execute(env *engine.VirtualEnvironment, command core.Command, actions []string, params *param.Parameters) error {
+	raw := params.JustGet("raw")
+	if raw == "true" {
+		output.Out(output.StdOut).Println(aux4.Version)
+		return nil
+	}
+
+	system := params.JustGet("system")
+	if system == "true" {
+		name, _ := os.Hostname()
+		output.Out(output.StdOut).Println(fmt.Sprintf("aux4/%s (%s; %s; %s)", aux4.Version, runtime.GOOS, runtime.GOARCH, name))
+		return nil
+	}
+
 	year := time.Now().Year()
 
 	output.Out(output.StdOut).Println()
@@ -29,4 +46,3 @@ func (executor *Aux4VersionExecutor) Execute(env *engine.VirtualEnvironment, com
 
 	return nil
 }
-
